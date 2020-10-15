5831c419eee639ddf8ebd003be4ca8ec0cd3d6c8
An Doan <dtan42@gmail.com>
2020-10-16 01:32:54 +1000

    Router + Context API

---
+++ src/StateProvider.js
@@ -0,0 +1,15 @@
+//set up data layer
+// we need this to track the basket
+
+import React, { createContext, useContext, useReducer } from 'react';
+
+// this is the data layer
+export const StateContext = createContext();
+//this is the data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,23 +4,45 @@ import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import { useState } from 'react';
-function Search() {
+import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
+import { actionTypes } from '../reducer'
+function Search({ hiddenButtons }) {
+    const [{ }, dispatch] = useStateValue()
     const [input, setInput] = useState("")
+    const history = useHistory()
     const search = e => {
         e.preventDefault();
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
+        setInput("")
+        history.push('/search')
     }
     return (
-        <div className="search">
+        <form className="search">
             <div className="search__input">
                 <SearchIcon className="search__inputIcon" />
                 <input type="text" value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
-            <div className="search__buttons">
-                <Button onClick={search} variant="outlined">Google Search</Button>
-                <Button variant="outlined">I'm Feeling Lucky</Button>
-            </div>
-        </div>
+            {
+                !hiddenButtons ? (
+                    <div className="search__buttons">
+                        <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
+                        <Button variant="outlined">I'm Feeling Lucky</Button>
+                    </div>
+                ) : (
+                        <div className="search__buttons">
+                            <Button type="submit" onClick={search} variant="outlined" className="search__buttonsHidden">Google Search</Button>
+                            <Button variant="outlined" className="search__buttonsHidden">I'm Feeling Lucky</Button>
+                        </div>
+                    )
+            }
+
+
+        </form>
     )
 }
 
